dd043489d747263cd95574fc14b83a9c4243a57f
ariostrynski13 <ariostrynski13@gmail.com>
2026-02-04 11:31:31 -0500

    Cinemachine Demo

--- Assets/Scripts/Mechanics/KillZone.cs
+++ Assets/Scripts/Mechanics/KillZone.cs
@@ -5,5 +5,6 @@ public class KillZone : Zone
     protected override void ApplyZoneEffect(Player player)
     {
         player.Die();
+        Debug.Log("Player has entered Kill Zone and died.");
     }
 }
